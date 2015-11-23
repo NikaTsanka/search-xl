11cc09fc2665f68e416e61c7ce48744ed2346c9f
NikaTsanka <nikatsanka@outlook.com>
2015-11-23 16:08:10 -0500

    Added sheet report function and also print function but it only prints 3 rows instead of all the rows.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import openpyxl
+"""
+Open and read the cells of an Excel document with the openpyxl module.
+Calculate all the tract and population data and store it in a data structure.
+Write the data structure to a text file with the .py extension using the pprint module.
+"""
+
+import openpyxl, pprint
+from openpyxl.utils import get_column_letter
 
 
 def get_files():
@@ -8,13 +15,44 @@ def get_files():
     sheets = wb.get_sheet_names()
     print(sheets)
 
-    sheet_name = input("Which sheet would you like to work with: ")
+    sheet_by_name = input("Which sheet would you like to work with: ")
 
-    if sheet_name in sheets:
-        sheet = wb.get_sheet_by_name(sheet_name)
-        for i in range(1, 8, 2):
-            print(i, sheet.cell(row=i, column=2).value)
+    if sheet_by_name in sheets:
+        sheet = wb.get_sheet_by_name(sheet_by_name)
 
+
+
+        for i in range(1, 4, 1):
+            print(sheet.cell(row=1, column=i).value, end=' ')
+
+    sheet = wb.get_sheet_by_name(sheet_by_name)
+
+    # get total number of rows and columns
+    rows = sheet.max_row
+    cols = sheet.max_column
+
+    # get max letters
+    # max_let_row = get_column_letter(sheet.max_row)
+    max_let_col = get_column_letter(sheet.max_column)
+
+    # print results
+    print("\nIn sheet: ", sheet_by_name, " There are: ", rows,
+          " rows and ", cols, " cols", sep='')
+
+    # convert to strings for args
+    start = 'A1'  # constant
+    end = str(max_let_col + str(cols))
+
+    # print values
+    for rowOfCellObjects in sheet[start:end]:
+        for cellObj in rowOfCellObjects:
+            print(cellObj.coordinate, cellObj.value)
+        print('----End of row----')
+"""
+# accept a worksheet
+def search_cells(work_sheet):
+    for cellObj in work_sheet[]:
+"""
 
 if __name__ == "__main__":
     get_files()
