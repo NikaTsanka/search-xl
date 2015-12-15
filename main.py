8e0c32ca8b490f4e5a83d6ba262d4e60d8039d33
NikaTsanka <nikatsanka@outlook.com>
2015-12-15 10:47:17 -0500

    Excel Package Update.

--- main.py
+++ main.py
@@ -10,17 +10,24 @@ from openpyxl.utils import get_column_letter
 
 def get_files():
     # open the workbook
-    wb = openpyxl.load_workbook('example.xlsx', read_only=True)
+    workbook = input("Please enter name of the workbook: ")
+    input_val = True
+    while input_val:
+        try:
+            wb = openpyxl.load_workbook(workbook, read_only=True)
+            input_val = False
+        except FileNotFoundError:
+            print("Specified workbook cannot be located.")
+            workbook = input("Please enter name of the workbook: ")
+
     print("Available Sheets")
     # print sheets
     sheets = wb.get_sheet_names()
     print(sheets)
-    # choose a sheet
-    # input("Which sheet would you like to work with: ")
 
     search_val = input("So what are you looking for? ")
 
-    sheet_by_name = "Sheet2"
+    sheet_by_name = input("Which sheet would you like to work with: ")
     # if chosen sheet is in the workbook then do the following
     if sheet_by_name in sheets:
         # set the sheet
@@ -38,8 +45,6 @@ def get_files():
                     for rVAl in range(j, col):
                         print("Name: ", sheet.cell(row=1, column=rVAl).value)
                         print(str(sheet.cell(row=j, column=rVAl).value))
-
-                        askUser = input("Continue? (y/n): ")
 
     # # set the sheet again
     # sheet = wb.get_sheet_by_name(sheet_by_name)
@@ -65,8 +70,6 @@ def get_files():
     #     for cellObj in rowOfCellObjects:
     #         print(cellObj.coordinate, cellObj.value)
     #     print('----End of row----')
-
-
 
 if __name__ == "__main__":
     get_files()
